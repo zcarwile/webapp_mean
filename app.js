cbc877b3bd908c79234994bc39896fb8bd186aee
Zach Carwile <zachary.carwile@gmail.com>
2016-03-22 17:11:27 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = angular.module('flapperNews', []);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+app.controller('MainCtrl', 
+  [
+    '$scope',
+    'posts',
+    function($scope, posts) {
+
+      $scope.posts = posts.posts;
+
+      $scope.addPost = function() {
+	if(!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
+	$scope.title = '';
+	$scope.link = '';
+      }
+
+      $scope.incrementUpvotes = function(post) {
+	post.upvotes += 1;
+      }
+
+      $scope.decrementUpvotes = function(post) {
+	post.upvotes -= 1;
+      }
+
+    }
+  ]
+);
