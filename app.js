7fa5c36c75302a6e9f5ef3545ba70998454a65f3
Zach Carwile <zachary.carwile@gmail.com>
2016-03-24 13:02:21 -0400

    got first states working -- need to understand

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+  $stateProvider
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
 
 app.factory('posts', [function(){
   var o = {
@@ -16,10 +37,15 @@ app.controller('MainCtrl',
       $scope.posts = posts.posts;
 
       $scope.addPost = function() {
-	if(!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link});
-	$scope.title = '';
-	$scope.link = '';
+    	if(!$scope.title || $scope.title === '') { return; }
+            $scope.posts.push({title: $scope.title, upvotes: 0, link: $scope.link,
+                  comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                  ]
+            });
+    	$scope.title = '';
+    	$scope.link = '';
       }
 
       $scope.incrementUpvotes = function(post) {
@@ -33,3 +59,11 @@ app.controller('MainCtrl',
     }
   ]
 );
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
